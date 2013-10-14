56c17e273eb71116509b3530c43783ea62ea8532
John Macdonald <jmacdevelopment@gmail.com>
2013-10-14 10:53:31 -0400

    Another change from different repos

--- hellofunc.c
+++ hellofunc.c
@@ -7,5 +7,6 @@ void myPrintHelloMake(void)
 	printf("Hello makefiles!\n");
 	printf ("this is a test of the Jenkins build system\n");
 	printf ("First...we test the github repository\n");
+	printf ("A test from a different working repos, same user\n");
 	return;
 }
