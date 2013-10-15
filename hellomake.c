2d116f3267ee2d0624562d4cf5053218ba58cb60
John Macdonald <john.macdonald@lsi.com>
2013-10-15 12:28:16 -0400

    Another commit on different file behind LSI firewall

--- hellomake.c
+++ hellomake.c
@@ -1,9 +1,17 @@
 #include "hellomake.h" 
+#include <stdio.h>
 
 int main() 
 {
-  // call a function in another file
+	int i=0;
+
+	// call a function in another file
 	myPrintHelloMake();
+
+	for ( i=0; i < 10; i++ )
+	{
+		printf ("i = %d\n", i);
+	}
 
 	return(0);
 }
